dee90151f59e322a087af0c2496409abfa499048
sarim <sarimk80.10p@gmail.com>
2020-02-26 17:26:18 +0500

    Initial Commit

---
+++ delete/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  delete
+//
+//  Created by Sarim Khan on 26/02/2020.
+//  Copyright © 2020 Sarim Khan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
